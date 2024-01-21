d0b788c71c23ef45d066a10d04031aea621f3923
陈庆敬 <c.q.j@outlook.com>
2024-01-21 18:24:17 +0800

    [Server]自定义二进制通讯协议编码解码

--- cruise_server/src/main/java/org/boketto/cruise_server/network/InboundMessage.java
+++ cruise_server/src/main/java/org/boketto/cruise_server/network/InboundMessage.java
@@ -2,6 +2,10 @@ package org.boketto.cruise_server.network;
 
 import lombok.Data;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.UUID;
+
 /**
  * 自定义二进制通讯协议，参考了如下方案，并在此基础上进行了适当调整：
  * https://www.cnblogs.com/caoweixiong/p/14663492.html
@@ -23,9 +27,20 @@ public class InboundMessage {
 
     private String messageDigest;
 
-    public byte[] decodeMessage() {
+    public InboundMessage decodeMessage() throws IOException {
         byte[] bytes = new byte[10000];
-        return bytes;
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        DataInputStream dataInputStream = new DataInputStream(byteArrayInputStream);
+        this.magicNumber = dataInputStream.readInt();
+        this.protocolVersion = dataInputStream.readInt();
+        byte[] temp = new byte[32];
+        dataInputStream.readNBytes(temp, 4, 32);
+        this.messageId = new String(temp, Charset.defaultCharset());
+        this.messageType = dataInputStream.readInt();
+        this.contentLength = dataInputStream.readInt();
+        dataInputStream.readNBytes(messageBody, 2+2+32+2+2, 68);
+        dataInputStream.readNBytes(new byte[32], 2+2+32+2+2+68, 32);
+        return this;
     }
 
 }
--- cruise_server/src/main/java/org/boketto/cruise_server/network/OutboundMessage.java
+++ cruise_server/src/main/java/org/boketto/cruise_server/network/OutboundMessage.java
@@ -2,6 +2,12 @@ package org.boketto.cruise_server.network;
 
 import lombok.Data;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.UUID;
+
 /**
  * 自定义二进制通讯协议，参考了如下方案，并在此基础上进行了适当调整：
  * https://www.cnblogs.com/caoweixiong/p/14663492.html
@@ -23,9 +29,23 @@ public class OutboundMessage {
 
     private String messageDigest;
 
-    public byte[] encodeMessage() {
+    public byte[] encodeMessage() throws IOException {
         byte[] bytes = new byte[10000];
-        return bytes;
+        this.messageId = UUID.randomUUID().toString().replaceAll("-", "");
+        this.magicNumber = 9527;
+        this.protocolVersion = 1;
+        this.messageBody = new byte[68];
+        this.messageDigest = "";
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        DataOutputStream dataOutputStream = new DataOutputStream(byteArrayOutputStream);
+        dataOutputStream.writeInt(magicNumber);
+        dataOutputStream.writeInt(protocolVersion);
+        dataOutputStream.write(messageId.getBytes(Charset.defaultCharset()));
+        dataOutputStream.writeInt(messageType);
+        dataOutputStream.writeInt(2 + 2 + 32 + 2 + 2 + 68 + 32);
+        dataOutputStream.write(messageBody);
+        dataOutputStream.write(new byte[32]);
+        return byteArrayOutputStream.toByteArray();
     }
 
 }
