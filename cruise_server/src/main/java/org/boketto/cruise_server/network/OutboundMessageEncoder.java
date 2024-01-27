5893dfc487435871a145887a9af5bd85c8a99e27
陈庆敬 <c.q.j@outlook.com>
2024-01-27 09:40:59 +0800

    [Server]二进制通讯协议继续与Netty整合

--- cruise_server/src/main/java/org/boketto/cruise_server/network/OutboundMessageEncoder.java
+++ cruise_server/src/main/java/org/boketto/cruise_server/network/OutboundMessageEncoder.java
@@ -1,5 +1,7 @@
 package org.boketto.cruise_server.network;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelOutboundHandlerAdapter;
 import io.netty.channel.ChannelPromise;
@@ -8,9 +10,12 @@ public class OutboundMessageEncoder extends ChannelOutboundHandlerAdapter {
 
     @Override
     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+        //Server端处理网络报文的第一个解码器便是LengthFieldBasedFrameDecoder，
+        //其入参只支持ByteBuf类型，所以客户端的出站编码器需要将数据处理为ByteBuf，
+        //具体可以看LengthFieldBasedFrameDecoder解码器类的decode方法实现
         byte[] bytes = ((OutboundMessage)msg).encodeMessage();
-        ctx.writeAndFlush(bytes);
-        super.write(ctx, msg, promise);
+        ByteBuf byteBuf = Unpooled.copiedBuffer(bytes);
+        ctx.writeAndFlush(byteBuf);
     }
 
 }
